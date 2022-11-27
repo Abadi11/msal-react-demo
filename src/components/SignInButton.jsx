1386ea892142c08e2a51ed6de68ef647ee54829d
Abadi11 <a.salman2040@gmail.com>
2022-11-27 10:43:22 +0000

    loging set up

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -1,8 +1,16 @@
 import React, { Component }  from 'react';
 import Button from '@mui/material/Button';
+import { useMsal } from '@azure/msal-react';
 
 export const SignInButton = () => {
+    const { instance } = useMsal();
+
+    const handleSignIn = () => {
+        instance.loginRedirect({
+            scopes: ['user.read']
+        })
+    }
     return (
-        <Button color="inherit">Sign in</Button>
+        <Button color="inherit" onClick={handleSignIn}>Sign in</Button>
     )
 };
