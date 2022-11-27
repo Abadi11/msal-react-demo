902d5c026bb9f32d479ed4bdefb1f8b8c56d9391
Abadi11 <a.salman2040@gmail.com>
2022-11-27 10:45:53 +0000

    addEventCallBack to get the user account details

--- src/index.js
+++ src/index.js
@@ -8,12 +8,22 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
-import { PublicClientApplication } from '@azure/msal-browser';
+import { PublicClientApplication, EventType } from '@azure/msal-browser';
 
 const pca = new PublicClientApplication({
-    clientId: '784e585f-2ddb-49d1-a710-9a3343bb3a47',
-    authority: 'https://login.microsoftonline.com/5a380cd7-791e-4b4c-aa94-ee0574864ac8',
-    redirectUri: '/'
+    auth: {
+        clientId: '784e585f-2ddb-49d1-a710-9a3343bb3a47',
+        authority: 'https://login.microsoftonline.com/5a380cd7-791e-4b4c-aa94-ee0574864ac8',
+        redirectUri: '/'
+    }
+    
+})
+
+pca.addEventCallback(event => {
+    console.log(event)
+    if (event.eventType === EventType.LOGIN_SUCCESS){
+        pca.setActiveAccount(event.payload.account)
+    }
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
